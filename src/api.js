84f8aae22222bbb86078bee28fb4a5241296a8fc
Uwihanganyeobed <uwihanganyeobed@gmail.com>
2025-08-29 11:29:45 +0200

    alls

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // eslint-disable-next-line no-undef
 const api_url = process.env.BACK_END_SERVICE_URL
 const API = axios.create({
-    baseURL: api_url ||'http://localhost:5000/'})
+    baseURL: api_url || 'https://mrn-back-end.onrender.com/'})
 
 //let's try some logic for interceptors if we have some tokens
 API.interceptors.request.use((req)=>{
