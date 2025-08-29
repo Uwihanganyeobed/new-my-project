10fd219f52c5179639ba3b684ea75fffcc858aa9
Uwihanganyeobed <uwihanganyeobed@gmail.com>
2025-08-29 10:58:10 +0200

    all-set

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+
+// eslint-disable-next-line no-undef
+const api_url = process.env.BACK_END_SERVICE_URL
 const API = axios.create({
-    baseURL: 'http://localhost:5000/'})
+    baseURL: api_url ||'http://localhost:5000/'})
 
 //let's try some logic for interceptors if we have some tokens
 API.interceptors.request.use((req)=>{
